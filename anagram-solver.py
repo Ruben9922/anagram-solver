f99b9db14dc872e4cc3123d4e65553f674689d2a
Ruben9922 <Ruben9922@users.noreply.github.com>
2019-08-22 21:48:11 +0100

    Create dictionary mapping character counts to sets of words

--- anagram-solver.py
+++ anagram-solver.py
@@ -1,7 +1,13 @@
-from collections import Counter
+from collections import Counter, defaultdict
 import re
 
 with open("words.txt") as f:
     words = f.read().splitlines()  # From: https://stackoverflow.com/a/3925701/3806231
 
     words_letters = [(word, Counter(re.sub(r"[^a-zA-Z0-9]", "", word.lower()))) for word in words]
+
+    letters_word_dict = defaultdict(set)
+    for item in words_letters:
+        letters_word_dict[item[1]].add(item[0])
+
+    print(letters_word_dict)
