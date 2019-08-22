501bb8eb66ed6b346434c1485cf2d06a0738d3fe
Ruben9922 <Ruben9922@users.noreply.github.com>
2019-08-22 15:53:59 +0100

    Ignore case and ignore non-alphanumeric characters

--- anagram-solver.py
+++ anagram-solver.py
@@ -1,6 +1,7 @@
 from collections import Counter
+import re
 
 with open("words.txt") as f:
     words = f.read().splitlines()  # From: https://stackoverflow.com/a/3925701/3806231
 
-    words_letters = [(word, Counter(word)) for word in words]
+    words_letters = [(word, Counter(re.sub(r"[^a-zA-Z0-9]", "", word.lower()))) for word in words]
