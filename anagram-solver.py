855ebe7d35e90d89fc2ca240c7930a545b6a062c
Ruben9922 <Ruben9922@users.noreply.github.com>
2019-08-22 21:54:43 +0100

    Fix unhashable type error by converting Counter object to frozenset

--- anagram-solver.py
+++ anagram-solver.py
@@ -4,7 +4,7 @@ import re
 with open("words.txt") as f:
     words = f.read().splitlines()  # From: https://stackoverflow.com/a/3925701/3806231
 
-    words_letters = [(word, Counter(re.sub(r"[^a-zA-Z0-9]", "", word.lower()))) for word in words]
+    words_letters = [(word, frozenset(Counter(re.sub(r"[^a-zA-Z0-9]", "", word.lower())).items())) for word in words]
 
     letters_word_dict = defaultdict(set)
     for item in words_letters:
