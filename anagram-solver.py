e3b32a744152b90bb353b85812933739af36ef1c
Ruben9922 <Ruben9922@users.noreply.github.com>
2019-08-22 15:14:57 +0100

    Rename variable

--- anagram-solver.py
+++ anagram-solver.py
@@ -1,3 +1,3 @@
 with open("words.txt") as f:
-    lines = f.read().splitlines()  # From: https://stackoverflow.com/a/3925701/3806231
-    print(lines)
+    words = f.read().splitlines()  # From: https://stackoverflow.com/a/3925701/3806231
+    print(words)
