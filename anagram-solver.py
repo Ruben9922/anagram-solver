abd6b998b1cf8c3224d8f6c56cbd048fb999e717
Ruben9922 <Ruben9922@users.noreply.github.com>
2019-08-22 15:44:37 +0100

    For each word, store number of occurrences of each letter in the word using a Counter

--- anagram-solver.py
+++ anagram-solver.py
@@ -1,2 +1,6 @@
+from collections import Counter
+
 with open("words.txt") as f:
     words = f.read().splitlines()  # From: https://stackoverflow.com/a/3925701/3806231
+
+    words_letters = [(word, Counter(word)) for word in words]
